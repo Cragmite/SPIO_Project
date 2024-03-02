8fa605be2fc477fe628e9ee8c996b98dd2c4816d
ruspio <149974705+ruspio@users.noreply.github.com>
2024-03-02 18:56:37 +0100

    Delete myapp directory
    Cleaning

--- myapp/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from .models import Machine
-
-class MachineSelectionForm(forms.Form):
-    machine = forms.ModelChoiceField(queryset=Machine.objects.all(), empty_label=None)
