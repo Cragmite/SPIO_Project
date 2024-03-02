18e4133549d74bec953ad873a533340b406fa2e1
Andrzej <cragmite@gmail.com>
2024-03-02 11:21:04 +0100

    Added 'Machine' database

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Machine(models.Model):
+    name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    area = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
