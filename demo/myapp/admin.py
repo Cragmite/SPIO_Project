baa80dfdf3a93523a800162fb1bb7c840b6964de
Andrzej <cragmite@gmail.com>
2024-03-02 12:39:43 +0100

    Added databases: Failure, Ticket

--- demo/myapp/admin.py
+++ demo/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TodoItem
 from .models import Machine
+from .models import Failure
+from .models import Ticket
 
 # Register your models here.
 admin.site.register(Machine)
